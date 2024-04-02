03f927d0f50f9f8a75ed85300f4f82b99cb2689f
C0dyS <sashaculle@gmail.com>
2024-04-02 20:22:46 +0200

    2 task done

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
-class Calculator:
-    @classmethod
-    def add(cls,x,y):
-        return x + y
-    @classmethod
-    def subtract(cls,x,y):
-        return x - y
-    @classmethod
-    def multiply(cls,x,y):
-        return x * y
-    @classmethod
-    def divide(cls,x,y):
-        if y == 0:
-            raise ValueError("Division by zero is not allowed")
-        else:
-            return x / y
-    @classmethod
-    def __call__(cls,operation_type,a,b):
-        if operation_type == 'add':
-            return cls.add(a,b)
-        elif operation_type == 'subtract':
-            return cls.subtract(a,b)
-        elif operation_type == 'multiply':
-            return cls.multiply(a,b)
-        elif operation_type == 'divide':
-            return cls.divide(a,b)
-        else:
-            raise ValueError('Wrong operation type')
+class FileWeightDescriptor:
 
-test_1 = Calculator()
-print(test_1('add',1,3))
-print(test_1('divide',1,3))
+    def __get__(self, instance, owner):
+        return instance._size
+
+    def __set__(self,instance,value):
+        if value < 0:
+            raise ValueError('file size is zero')
+        elif not isinstance(value,int):
+                raise ValueError('wrong variable type')
+        else: instance._size = value
+
+    def format_size(self, size):
+        if size < 1024:
+            return f'{size} bytes'
+        elif size < 1024 ** 2:
+            return f'{size / 1024:.2f} KB'
+        else:
+            return f'{size / (1024 ** 2):.2f} MB'
+
+class File:
+    size = FileWeightDescriptor()
+    def __init__(self,size):
+        self._size = size
+
+file1 = File(1024)
+print(file1.size)
+print(FileWeightDescriptor().format_size(file1.size))
+
